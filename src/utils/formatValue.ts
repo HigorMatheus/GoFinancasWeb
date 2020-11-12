b06342aebea2008ca41ec34bd2090df90bd0bec7
Higor Matheus O Vieira <hmoliveira83@gmail.com>
2020-11-12 16:16:59 -0300

    finalizando desafio

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,15 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+// const formatValue = (value: number): string =>
+//   Intl.NumberFormat().format(value); // TODO
+
+// export default formatValue;
+
+function formatValue(value: number): string {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(value);
+}
 
 export default formatValue;
